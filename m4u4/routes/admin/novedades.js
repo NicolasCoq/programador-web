d2b3a83c33bb07fd71678e352416a433c7658666
Nicolas Coquoz <coquoznicolas2002@gmail.com>
2026-02-10 16:51:37 -0300

    m5u1

--- m4u4/routes/admin/novedades.js
+++ m4u4/routes/admin/novedades.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var novedadesModel = require('../../models/novedadesModel')
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('admin/novedades',{
+/* Listar */
+router.get('/', async function (req, res, next) {
+
+  var novedades = await novedadesModel.getNovedades();
+
+  res.render('admin/novedades', {
     layout: 'admin/layout',
-    usuario: req.session.nombre
+    usuario: req.session.nombre,
+    novedades
   });
+});
+
+/* para borrar */
+router.get('/eliminar/:id', async (req, res, next) => {
+  const id = req.params.id;
+
+  //console.log(id)
+
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
+
 });
 
 module.exports = router;
